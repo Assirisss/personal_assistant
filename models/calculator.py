bbc3d1f73439daeee65f07dcf60a4823208cbdfc
1111 <a1111@Nikita.local>
2024-11-21 19:38:56 +0300

    change support, ex

--- models/calculator.py
+++ models/calculator.py
@@ -4,26 +4,26 @@ from curses.ascii import isdigit
 class Calculator:
     def add(self, a, b):
         try:
-            return a + b
+            return int(a) + int(b)
         except TypeError:
             return "Invalid input"
 
     def subtract(self, a, b):
         try:
-            return a - b
+            return int(a) - int(b)
         except TypeError:
             return "Invalid input"
 
     def multiply(self, a, b):
-        if isdigit(a) and isdigit(b):
-            return a * b
+        if a.isdigit() and b.isdigit():
+            return int(a) * int(b)
         else:
             return "Invalid input"
 
     def divide(self, a, b):
-        if isdigit(a) and isdigit(b):
-            if b != 0:
-                return a / b
+        if a.isdigit() and b.isdigit():
+            if b != '0':
+                return int(a) / int(b)
             else:
                 return "Division by zero"
         else:
